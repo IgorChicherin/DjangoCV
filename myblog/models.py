1eceefcf774508505fa28391a9e93d71cb715811
Igor Chicherin <ashzak990@gmail.com>
2018-06-18 14:06:37 +0300

    easy thumbnails model

--- myblog/models.py
+++ myblog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from easy_thumbnails.fields import ThumbnailerImageField
 
 
 
 # Create your models here.
 
 class Portfolio(models.Model):
-    img = models.ImageField(upload_to='static/img', null=True, )
+    img = ThumbnailerImageField(upload_to='portfolio', null=True, blank=True)
     work_name = models.CharField(max_length=100)
     link = models.URLField(max_length=250, null=True)
     text = models.TextField()
@@ -14,9 +17,13 @@ class Portfolio(models.Model):
     def __str__(self):
         return str(self.work_name)
 
+# @receiver(pre_save, Portfolio)
+# def fix_path()
+
+
 
 class Education(models.Model):
-    img = models.ImageField(upload_to='static/img')
+    img = ThumbnailerImageField(upload_to='education', null=True, blank=True)
     course = models.CharField(max_length=100)
     text = models.TextField()
     tag = models.TextField(max_length=50)
@@ -37,7 +44,7 @@ class Company(models.Model):
 
 class Work(models.Model):
     position = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='static/img')
+    img = ThumbnailerImageField(upload_to='work', null=True, blank=True)
     company = models.ForeignKey(Company, null=True)
     link = models.URLField(max_length=250)
     text = models.TextField()
